74de5661a02543243fe8cdc4ba0b0cb954f07013
Gergo Torcsvari <torcsvari.gergo@gmail.com>
2020-08-03 23:41:39 +0200

    better error handling

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from pycoingecko import CoinGeckoAPI
-import os, json, crython
+import os, json, crython, traceback
 from paho.mqtt import publish
 
 print("started")
 topic = os.environ.get('MQTT_TOPIC')
 host = os.environ.get('MQTT_HOST')
-port = os.environ.get('MQTT_PORT')
+port = int(os.getenv('MQTT_PORT', '1883'))
 
 currencies = os.environ.get('CURRENCIES')
 vs_currencies = os.environ.get('VS_CURRENCIES')
 
 schedule = os.getenv('SCHEDULE')
+cg = CoinGeckoAPI()
 
 @crython.job(expr=schedule)
 def process():
-    cg = CoinGeckoAPI()
-    data = cg.get_price(ids=currencies, vs_currencies=vs_currencies)
-    jsonString = json.dumps(data)
-    print(jsonString)
-    publish.single(topic, jsonString, hostname=host, port=port)
+    try:
+        data = cg.get_price(ids=currencies, vs_currencies=vs_currencies)
+        jsonString = json.dumps(data)
+        print(jsonString)
+        publish.single(topic, jsonString, hostname=host, port=port)
+    except Exception as e:
+        print("Unknown error:" + str(e))
+        traceback.print_exc()
 
 
 if __name__ == "__main__":
